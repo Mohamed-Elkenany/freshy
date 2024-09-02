b4746d3916b610a1cceca474e09b15ee66fb0fb5
unknown <elkenani44@gmail.com>
2024-09-03 02:10:48 +0300

    some update

--- js-file/freshy.js
+++ js-file/freshy.js
@@ -1,33 +1,38 @@
 /*
 New Update
 */
-
+let IconMenu = document.querySelector(".header .IconMenu");
+let menuList = document.querySelector(".header .menuList");
+IconMenu.addEventListener("click", (e) => {
+  menuList.classList.toggle("active");
+});
 let menuContent = document.querySelector(".menu-content");
 let dirMove = document.querySelectorAll(".direction i");
 let firstBox = menuContent.querySelectorAll(".menu-box")[0];
 let firstBoxWidth = firstBox.clientWidth + 15;
-let isMove = false, prevPageX, prevScrollLeft;
+let isMove = false,
+  prevPageX,
+  prevScrollLeft;
 let isSMove = (e) => {
-    isMove = true;
-    prevPageX = e.pageX;
-    prevScrollLeft = menuContent.scrollLeft;
-
-}
-dirMove.forEach(i => {
-    i.addEventListener("click", () => {
-        menuContent.scrollLeft += i.id == "left" ? firstBoxWidth : -firstBoxWidth;
-        menuContent.style.scrollBehavior = "smooth";
-    });
+  isMove = true;
+  prevPageX = e.pageX;
+  prevScrollLeft = menuContent.scrollLeft;
+};
+dirMove.forEach((i) => {
+  i.addEventListener("click", () => {
+    menuContent.scrollLeft += i.id == "left" ? firstBoxWidth : -firstBoxWidth;
+    menuContent.style.scrollBehavior = "smooth";
+  });
 });
 let scrollMove = (e) => {
-    if (!isMove) return;
-    let postionDiff = e.pageX - prevPageX;
-    menuContent.scrollLeft = prevScrollLeft - postionDiff;
-    e.preventDefault();
-}
+  if (!isMove) return;
+  let postionDiff = e.pageX - prevPageX;
+  menuContent.scrollLeft = prevScrollLeft - postionDiff;
+  e.preventDefault();
+};
 let isStopmove = () => {
-    isMove = false;
-}
+  isMove = false;
+};
 menuContent.addEventListener("mousedown", isSMove);
 menuContent.addEventListener("mousemove", scrollMove);
 menuContent.addEventListener("mouseup", isStopmove);
@@ -39,45 +44,39 @@ let loginForm = document.querySelector(".login-form");
 let signupForm = document.querySelector(".signup-form");
 
 signup.onclick = function () {
-    loginForm.classList.add("hidden");
-    signupForm.classList.remove("hidden");
-}
+  loginForm.classList.add("hidden");
+  signupForm.classList.remove("hidden");
+};
 logIn.onclick = function () {
-    signupForm.classList.add("hidden");
-    loginForm.classList.remove("hidden");
-}
+  signupForm.classList.add("hidden");
+  loginForm.classList.remove("hidden");
+};
 let layout = document.querySelector(".overlay");
 let signIn = document.querySelector(".header-btn");
 let eyeHeddin = document.querySelectorAll(".overlay .eye-icon");
 let inputType = document.querySelectorAll(".input-field input");
 
-
-eyeHeddin.forEach(eye => {
-    eye.addEventListener("click", (e) => {
-        e.target.parentElement.parentElement.querySelectorAll(".input-field input").forEach(pw => {
-            if (pw.type === 'password') {
-                pw.type = 'text';
-                eye.classList.replace("fa-eye-slash", "fa-eye");
-                return;
-            }
-            pw.type = 'password';
-            eye.classList.replace("fa-eye","fa-eye-slash");
-        });
-    });
+eyeHeddin.forEach((eye) => {
+  eye.addEventListener("click", (e) => {
+    e.target.parentElement.parentElement
+      .querySelectorAll(".input-field input")
+      .forEach((pw) => {
+        if (pw.type === "password") {
+          pw.type = "text";
+          eye.classList.replace("fa-eye-slash", "fa-eye");
+          return;
+        }
+        pw.type = "password";
+        eye.classList.replace("fa-eye", "fa-eye-slash");
+      });
+  });
 });
 signIn.onclick = function () {
-    layout.style.top = '0';
-}
-let removeOvarlay = document.querySelectorAll(".remove-overlay");
-removeOvarlay.forEach(element => {
-    element.addEventListener("click", () => {
-        layout.style.top = '-2000px';
-    });
-});
-let navBar = document.querySelector(".header .navbar");
-let menuBar = document.querySelector(".header .menu-bar");
-let move = document.querySelector(".header .move");
-menuBar.onclick = function () {
-    navBar.classList.toggle("active");
+  layout.style.top = "0";
 };
-
+let removeOvarlay = document.querySelectorAll(".remove-overlay");
+removeOvarlay.forEach((element) => {
+  element.addEventListener("click", () => {
+    layout.style.top = "-2000px";
+  });
+});
